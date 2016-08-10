c58604fb80226b3a7bf32f9eaad0a0b83163a85b
tingwei628 <sbvq6p199xe@gmail.com>
2016-08-10 17:21:12 +0800

    add todo with Redux

---
+++ src/TodoWithReduxApp/TodoWithReduxApp.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+export default class TodoWithReduxApp extends Component {
+  render() {
+    return (
+      <div>
+        Todo List
+      </div>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -3,12 +3,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import App from './App';
 import ChatWithFluxApp from './ChatWithFluxApp';
-import { init } from './ChatWithFluxApp/ChatExampleData';
-import { getAllMessages } from './ChatWithFluxApp/utils/ChatAppUtils';
+import TodoWithReduxApp from './TodoWithReduxApp/TodoWithReduxApp';
+// import { init } from './ChatWithFluxApp/ChatExampleData';
+// import { getAllMessages } from './ChatWithFluxApp/utils/ChatAppUtils';
 
-init();
-getAllMessages();
-fetch('https://guarded-brushlands-26579.herokuapp.com/api/contacts').then((res) => 
-  res.json().then((data) => console.log(data))
-);
-ReactDOM.render(<ChatWithFluxApp />, document.getElementById('root'));
+// init();
+// getAllMessages();
+
+ReactDOM.render(<TodoWithReduxApp />, document.getElementById('root'));
